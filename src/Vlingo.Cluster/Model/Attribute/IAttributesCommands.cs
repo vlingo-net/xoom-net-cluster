44837a462e524076265e06f1afdd8358bd8995e5
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-04-14 15:43:13 +0200

    Add IAttributesCommands, IAttributesQueries and IAttributesProtocol interfaces

---
+++ src/Vlingo.Cluster/Model/Attribute/IAttributesCommands.cs
@@ -0,0 +1,20 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Cluster.Model.Attribute
+{
+    public interface IAttributesCommands
+    {
+        void Add<T>(string attributeSetName, string attributeName, T value);
+        
+        void Replace<T>(string attributeSetName, string attributeName, T value);
+        
+        void Remove(string attributeSetName, string attributeName);
+        
+        void RemoveAll(string attributeSetName);
+    }
+}
