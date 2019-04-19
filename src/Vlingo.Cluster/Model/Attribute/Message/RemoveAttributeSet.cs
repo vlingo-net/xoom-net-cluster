8692b8af4a48f47c4b747b9266e3b84220bd86ce
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-04-19 21:46:27 +0200

    Implement RemoveAttributeSet

---
+++ src/Vlingo.Cluster/Model/Attribute/Message/RemoveAttributeSet.cs
@@ -0,0 +1,45 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Text;
+
+namespace Vlingo.Cluster.Model.Attribute.Message
+{
+    using Vlingo.Wire.Node;
+    
+    public sealed class RemoveAttributeSet : ApplicationMessage
+    {
+        public static RemoveAttributeSet From(Node node, AttributeSet set) => new RemoveAttributeSet(node, set);
+        
+        public RemoveAttributeSet(Node node, AttributeSet set)
+            : base(
+                NoCorrelatingMessageId,
+                ApplicationMessageType.RemoveAttributeSet,
+                TrackingIdFor(node, ApplicationMessageType.CreateAttributeSet, set.Name))
+        {
+            AttributeSetName = set.Name;
+        }
+        
+        public string AttributeSetName { get; }
+
+        public override string ToPayload()
+        {
+            var builder = new StringBuilder();
+            
+            builder
+                .Append(GetType().Name)
+                .Append("\n")
+                .Append(TrackingId)
+                .Append("\n")
+                .Append(Type.ToString())
+                .Append("\n")
+                .Append(AttributeSetName);
+    
+            return builder.ToString();
+        }
+    }
+}
