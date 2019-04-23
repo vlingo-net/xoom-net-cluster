e44f9b29cf68085a5298afa03b2f8165d3bcb581
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-04-24 00:06:52 +0200

    Implement NodeBootstrap and ShutdownHook

---
+++ src/Vlingo.Cluster/ShutdownHook.cs
@@ -0,0 +1,50 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Threading;
+using Vlingo.Actors;
+using Vlingo.Cluster.Model;
+
+namespace Vlingo.Cluster
+{
+    internal sealed class ShutdownHook
+    {
+        private readonly Tuple<IClusterSnapshotControl, ILogger> _control;
+        private readonly string _nodeName;
+
+        internal ShutdownHook(string nodeName, Tuple<IClusterSnapshotControl, ILogger> control)
+        {
+            _nodeName = nodeName;
+            _control = control;
+        }
+
+        internal void Register()
+        {
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                _control.Item2.Log("\n==========");
+                _control.Item2.Log($"Stopping node: '{_nodeName}' ...");
+                _control.Item1.ShutDown();
+                Pause();
+                _control.Item2.Log($"Stopped node: '{_nodeName}'");
+            };
+        }
+        
+        private void Pause()
+        {
+            try
+            {
+                Thread.Sleep(1000);
+            }
+            catch 
+            {
+                // ignore
+            }
+        }
+    }
+}
